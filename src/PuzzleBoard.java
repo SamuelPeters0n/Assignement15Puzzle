d22dfd37ab99f4538bad6b3c90ac3ddd38f8937b
SamuelPeters0n <sampet02@gmail.com>
2022-10-28 12:31:17 +0200

    Skapar spelplanen.

---
+++ src/PuzzleBoard.java
@@ -0,0 +1,37 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.util.ArrayList;
+
+public class PuzzleBoard extends JFrame implements ActionListener {
+
+    JPanel p = new JPanel();
+    ArrayList<ButtonWithPositionVariable> allButtons = ButtonWithPositionVariable.createButtonsArrayList();
+
+    PuzzleBoard() {
+
+        for (JButton element : allButtons) {
+            element.setPreferredSize(new Dimension(100, 100));
+            element.addActionListener(this);
+            p.add(element);
+        }
+        p.setLayout(new GridLayout(4, 4));
+        add(p);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setVisible(true);
+    }
+
+    public static void main(String[] arg) {
+        PuzzleBoard a = new PuzzleBoard();
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
