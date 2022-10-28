8cab1c6308a3452c061ba262fdaf194ba32426b4
Robin Ter-Borch <robin.ter-borch@yh.nackademin.se>
2022-10-28 11:20:59 +0200

    Start test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Testing
+        System.out.println("Test nummer 1");
+
+
+
     }
 }
